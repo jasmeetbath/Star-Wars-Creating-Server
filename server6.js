5696d506182cd7ea3ab29fea1b477509c7df7878
Jasmeet Bath <jasmeetbeth@user3-225-194.wireless.utoronto.ca>
2019-04-30 13:48:02 -0400

    updating server6.js

--- server6.js
+++ server6.js
@@ -1,19 +1,12 @@
-// Dependencies
-// =============================================================
 var express = require("express");
 var path = require("path");
 
-// Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = 3000;
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Star Wars Characters (DATA)
-// =============================================================
 var characters = [
   {
     routeName: "yoda",
@@ -38,10 +31,6 @@ var characters = [
   }
 ];
 
-// Routes
-// =============================================================
-
-// Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
   // res.send("Welcome to the Star Wars Page!")
   res.sendFile(path.join(__dirname, "view.html"));
@@ -67,7 +56,6 @@ app.get("/api/characters/:character", function(req, res) {
   return res.json(false);
 });
 
-// Create New Characters - takes in JSON input
 app.post("/api/characters", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
@@ -75,15 +63,12 @@ app.post("/api/characters", function(req, res) {
 
   console.log(newcharacter);
 
-  // We then add the json the user sent to the character array
   characters.push(newcharacter);
 
   // We then display the JSON to the users
   res.json(newcharacter);
 });
 
-// Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
